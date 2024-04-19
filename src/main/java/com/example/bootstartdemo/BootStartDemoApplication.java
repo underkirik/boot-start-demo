6ef867d67b15e91d701c83edfa5bbeee0ee6527f
Kirill Kopanchikov <kopanchikov98@gmail.com>
2024-04-19 23:48:48 +0300

    initial commit

---
+++ src/main/java/com/example/bootstartdemo/BootStartDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootstartdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStartDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootStartDemoApplication.class, args);
+	}
+
+}
